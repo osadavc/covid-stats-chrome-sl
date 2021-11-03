4c1007df3b07fd64456f2caa80ca8bb142ad772b
osadavc <osadavidath@gmail.com>
2021-11-03 17:44:52 +0530

    changed font

--- src/App.js
+++ src/App.js
@@ -15,35 +15,36 @@ const App = () => {
       })
       .catch((err) => console.log(err));
   }, []);
+
   return (
-    <div className="flex flex-col justify-center items-center min-w-[340px] max-w-[310px] min-h-[500px] bg-gray-100 relative">
+    <div className="flex flex-col justify-center items-center min-w-[340px] max-w-[310px] min-h-[500px] bg-gray-100 relative font-lato">
       {prevCount && (
-        <div className="absolute top-10 text-sm">
+        <div className="absolute top-10 text-md w-[10.3rem] text-center ">
           {prevCount == covidToday?.local_new_cases && (
             <h1 className="font-bold">Nothing New Here, Not Worth Checking</h1>
           )}
           {prevCount > covidToday?.local_new_cases && (
             <h1 className="text-gray-700 font-bold">
-              Oops it is lower than last time
+              Oops!! it is lower than last time
             </h1>
           )}
           {prevCount < covidToday?.local_new_cases && (
             <h1 className="text-indigo-700 font-extrabold">
-              Interesting it is higher than last time
+              Interesting, it is higher than last time
             </h1>
           )}
         </div>
       )}
 
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="flex justify-center items-center mb-10 bg-gray-200 py-3 px-5 rounded-lg shadow-sm cursor-pointer space-x-2">
+        <div className="flex justify-center items-center mb-10 bg-gray-200 py-3 rounded-lg shadow-sm cursor-pointer space-x-2 w-[10.3rem]">
           <img
             src="https://hpb.health.gov.lk/covid19-dashboard/img/icon-4.d002c132.gif"
             className="w-20 h-20"
           />
           <h1 className="text-lg">{covidToday?.local_new_cases}</h1>
         </div>
-        <div className="flex justify-center items-center bg-gray-200 py-3 px-6 rounded-lg shadow-sm cursor-pointer space-x-2">
+        <div className="flex justify-center items-center bg-gray-200 py-3 w-[10.3rem] rounded-lg shadow-sm cursor-pointer space-x-2">
           <img
             src="https://hpb.health.gov.lk/covid19-dashboard/img/icon-1.403b9d8b.gif"
             className="w-20 h-20"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+// c-spell:disable
+
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
     extend: {},
+    fontFamily: {
+      lato: ["Lato"],
+    },
   },
   variants: {
     extend: {},
